42f242c8875ad4708a4fd1c11bc7fa7231f551a5
Doug Goodwin <dgoodwin@gmail.com>
2024-09-24 10:38:11 -0700

    Python3

--- utils/make_mosaic.py
+++ utils/make_mosaic.py
@@ -11,13 +11,15 @@ def find_rectangle(n, max_ratio=2):
         sides.append((leftover, (w, h)))
     return sorted(sides)[0][1]
 
-# should work for 1d and 2d images, assumes images are square but can be overriden
 def make_mosaic(images, n=None, nx=None, ny=None, w=None, h=None):
+    print("Let's make a MOSAIC!")
     if n is None and nx is None and ny is None:
         nx, ny = find_rectangle(len(images))
     else:
         nx = n if nx is None else nx
         ny = n if ny is None else ny
+    print(nx, ny)
+    
     images = np.array(images)
     if images.ndim == 2:
         side = int(np.sqrt(len(images[0])))
@@ -27,13 +29,26 @@ def make_mosaic(images, n=None, nx=None, ny=None, w=None, h=None):
     else:
         h = images.shape[1]
         w = images.shape[2]
+    
+    print(f"h, w is {h}, {w}")
+    print(f"images has len {len(images)}")
+    
     image_gen = iter(images)
-    mosaic = np.empty((h*ny, w*nx))
+    mosaic = np.empty((h * ny, w * nx))
+    
     for i in range(ny):
-        ia = (i)*h
-        ib = (i+1)*h
+        ia = i * h
+        ib = (i + 1) * h
         for j in range(nx):
-            ja = j*w
-            jb = (j+1)*w
-            mosaic[ia:ib, ja:jb] = next(image_gen)
+            ja = j * w
+            jb = (j + 1) * w
+            print(f"ia, ib {ia}, {ib}  ja, jb {ja}, {jb}")
+            try:
+                mosaic[ia:ib, ja:jb] = next(image_gen)
+            except StopIteration:
+                print("No more images available to complete the mosaic.")
+                break
+            except Exception as e:
+                print(f"Unexpected exception: {e}")
+    
     return mosaic
