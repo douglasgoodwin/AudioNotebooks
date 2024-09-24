6e644553e04a5e0dbc28d3a817378c20d83005eb
Doug Goodwin <dgoodwin@gmail.com>
2024-09-24 10:36:58 -0700

    Python3

--- utils/bhtsne.py
+++ utils/bhtsne.py
@@ -1,43 +1,5 @@
 #!/usr/bin/env python
 
-'''
-A simple Python wrapper for the bh_tsne binary that makes it easier to use it
-for TSV files in a pipeline without any shell script trickery.
-
-Note: The script does some minimal sanity checking of the input, but don't
-    expect it to cover all cases. After all, it is a just a wrapper.
-
-Example:
-
-    > echo -e '1.0\t0.0\n0.0\t1.0' | ./bhtsne.py -d 2 -p 0.1
-    -2458.83181442  -6525.87718385
-    2458.83181442   6525.87718385
-
-The output will not be normalised, maybe the below one-liner is of interest?:
-
-    python -c 'import numpy;  from sys import stdin, stdout; 
-        d = numpy.loadtxt(stdin); d -= d.min(axis=0); d /= d.max(axis=0);
-        numpy.savetxt(stdout, d, fmt="%.8f", delimiter="\t")'
-
-Authors:     Pontus Stenetorp    <pontus stenetorp se>
-             Philippe Remy       <github: philipperemy>
-Version:    2016-03-08
-'''
-
-# Copyright (c) 2013, Pontus Stenetorp <pontus stenetorp se>
-#
-# Permission to use, copy, modify, and/or distribute this software for any
-# purpose with or without fee is hereby granted, provided that the above
-# copyright notice and this permission notice appear in all copies.
-#
-# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-
 from argparse import ArgumentParser, FileType
 from os.path import abspath, dirname, isfile, join as path_join
 from shutil import rmtree
@@ -49,38 +11,29 @@ from platform import system
 from os import devnull
 import numpy as np
 
-### Constants
-IS_WINDOWS = True if system() == 'Windows' else False
+# Constants
+IS_WINDOWS = system() == 'Windows'
 BH_TSNE_BIN_PATH = path_join(dirname(__file__), 'windows', 'bh_tsne.exe') if IS_WINDOWS else path_join(dirname(__file__), 'bh_tsne')
-assert isfile(BH_TSNE_BIN_PATH), ('Unable to find the bh_tsne binary in the '
-    'same directory as this script, have you forgotten to compile it?: {}'
-    ).format(BH_TSNE_BIN_PATH)
+assert isfile(BH_TSNE_BIN_PATH), f'Unable to find the bh_tsne binary in the same directory as this script, have you forgotten to compile it?: {BH_TSNE_BIN_PATH}'
+
 # Default hyper-parameter values from van der Maaten (2014)
-# https://lvdmaaten.github.io/publications/papers/JMLR_2014.pdf (Experimental Setup, page 13)
 DEFAULT_NO_DIMS = 2
 INITIAL_DIMENSIONS = 50
 DEFAULT_PERPLEXITY = 50
 DEFAULT_THETA = 0.5
 EMPTY_SEED = -1
 
-###
-
 def _argparse():
-    argparse = ArgumentParser('bh_tsne Python wrapper')
-    argparse.add_argument('-d', '--no_dims', type=int,
-                          default=DEFAULT_NO_DIMS)
-    argparse.add_argument('-p', '--perplexity', type=float,
-            default=DEFAULT_PERPLEXITY)
-    # 0.0 for theta is equivalent to vanilla t-SNE
-    argparse.add_argument('-t', '--theta', type=float, default=DEFAULT_THETA)
+    argparse = ArgumentParser(description='bh_tsne Python wrapper')
+    argparse.add_argument('-d', '--no_dims', type=int, default=DEFAULT_NO_DIMS)
+    argparse.add_argument('-p', '--perplexity', type=float, default=DEFAULT_PERPLEXITY)
+    argparse.add_argument('-t', '--theta', type=float, default=DEFAULT_THETA)  # 0.0 for theta is equivalent to vanilla t-SNE
     argparse.add_argument('-r', '--randseed', type=int, default=EMPTY_SEED)
     argparse.add_argument('-n', '--initial_dims', type=int, default=INITIAL_DIMENSIONS)
     argparse.add_argument('-v', '--verbose', action='store_true')
     argparse.add_argument('-i', '--input', type=FileType('r'), default=stdin)
-    argparse.add_argument('-o', '--output', type=FileType('w'),
-            default=stdout)
+    argparse.add_argument('-o', '--output', type=FileType('w'), default=stdout)
     return argparse
-
 
 class TmpDir:
     def __enter__(self):
@@ -90,18 +43,17 @@ class TmpDir:
     def __exit__(self, type, value, traceback):
         rmtree(self._tmp_dir_path)
 
-
 def _read_unpack(fmt, fh):
     return unpack(fmt, fh.read(calcsize(fmt)))
 
-def bh_tsne(samples, no_dims=DEFAULT_NO_DIMS, initial_dims=INITIAL_DIMENSIONS, perplexity=DEFAULT_PERPLEXITY,
+def bh_tsne(samples, no_dims=DEFAULT_NO_DIMS, initial_dims=INITIAL_DIMENSIONS, perplexity=DEFAULT_PERPLEXITY, 
             theta=DEFAULT_THETA, randseed=EMPTY_SEED, verbose=False):
 
     samples -= np.mean(samples, axis=0)
     cov_x = np.dot(np.transpose(samples), samples)
-    [eig_val, eig_vec] = np.linalg.eig(cov_x)
+    eig_val, eig_vec = np.linalg.eig(cov_x)
 
-    # sorting the eigen-values in the descending order
+    # sorting the eigen-values in descending order
     eig_vec = eig_vec[:, eig_val.argsort()[::-1]]
 
     if initial_dims > len(eig_vec):
@@ -111,79 +63,56 @@ def bh_tsne(samples, no_dims=DEFAULT_NO_DIMS, initial_dims=INITIAL_DIMENSIONS, p
     eig_vec = eig_vec[:, :initial_dims]
     samples = np.dot(samples, eig_vec)
 
-    # Assume that the dimensionality of the first sample is representative for
-    #   the whole batch
     sample_dim = len(samples[0])
     sample_count = len(samples)
 
-    # bh_tsne works with fixed input and output paths, give it a temporary
-    #   directory to work in so we don't clutter the filesystem
     with TmpDir() as tmp_dir_path:
-        # Note: The binary format used by bh_tsne is roughly the same as for
-        #   vanilla tsne
         with open(path_join(tmp_dir_path, 'data.dat'), 'wb') as data_file:
             # Write the bh_tsne header
             data_file.write(pack('iiddi', sample_count, sample_dim, theta, perplexity, no_dims))
             # Then write the data
             for sample in samples:
-                data_file.write(pack('{}d'.format(len(sample)), *sample))
+                data_file.write(pack(f'{len(sample)}d', *sample))
             # Write random seed if specified
             if randseed != EMPTY_SEED:
                 data_file.write(pack('i', randseed))
 
-        # Call bh_tsne and let it do its thing
         with open(devnull, 'w') as dev_null:
-            bh_tsne_p = Popen((abspath(BH_TSNE_BIN_PATH), ), cwd=tmp_dir_path, close_fds=True)
+            bh_tsne_p = Popen([abspath(BH_TSNE_BIN_PATH)], cwd=tmp_dir_path, close_fds=True)
             bh_tsne_p.wait()
-            assert not bh_tsne_p.returncode, ('ERROR: Call to bh_tsne exited '
-                    'with a non-zero return code exit status, please ' +
-                    ('enable verbose mode and ' if not verbose else '') +
-                    'refer to the bh_tsne output for further details')
+            assert not bh_tsne_p.returncode, (
+                'ERROR: Call to bh_tsne exited with a non-zero return code exit status, please '
+                f'{"enable verbose mode and " if not verbose else ""}refer to the bh_tsne output for further details'
+            )
 
-        # Read and pass on the results
         with open(path_join(tmp_dir_path, 'result.dat'), 'rb') as output_file:
-            # The first two integers are just the number of samples and the
-            #   dimensionality
             result_samples, result_dims = _read_unpack('ii', output_file)
-            # Collect the results, but they may be out of order
-            results = [_read_unpack('{}d'.format(result_dims), output_file)
-                for _ in xrange(result_samples)]
-            # Now collect the landmark data so that we can return the data in
-            #   the order it arrived
+            results = [_read_unpack(f'{result_dims}d', output_file) for _ in range(result_samples)]
             results = [(_read_unpack('i', output_file), e) for e in results]
-            # Put the results in order and yield it
             results.sort()
             for _, result in results:
                 yield result
-            # The last piece of data is the cost for each sample, we ignore it
-            #read_unpack('{}d'.format(sample_count), output_file)
 
 def main(args):
     argp = _argparse().parse_args(args[1:])
 
-    # Read the data, with some sanity checking
     data = []
-    for sample_line_num, sample_line in enumerate((l.rstrip('\n')
-            for l in argp.input), start=1):
-        sample_data = sample_line.split('\t')
-        try:
-            assert len(sample_data) == dims, ('Input line #{} of '
-                    'dimensionality {} although we have previously observed '
-                    'lines with dimensionality {}, possible data error or is '
-                    'the data sparsely encoded?'
-                    ).format(sample_line_num, len(sample_data), dims)
-        except NameError:
-            # First line, record the dimensionality
+    try:
+        for sample_line in argp.input:
+            sample_data = list(map(float, sample_line.rstrip().split('\t')))
+            if 'dims' in locals() and len(sample_data) != dims:
+                raise ValueError(f"Input line has dimensionality {len(sample_data)} but expected {dims}")
             dims = len(sample_data)
-        data.append([float(e) for e in sample_data])
+            data.append(sample_data)
+    except ValueError as e:
+        print(f"Error processing input data: {e}", file=stderr)
+        return 1
 
-    for result in bh_tsne(data, no_dims=argp.no_dims, perplexity=argp.perplexity, theta=argp.theta, randseed=argp.randseed,
-            verbose=argp.verbose, initial_dims=argp.initial_dims):
-        fmt = ''
-        for i in range(1, len(result)):
-            fmt = fmt + '{}\t'
-        fmt = fmt + '{}\n'
-        argp.output.write(fmt.format(*result))
+    for result in bh_tsne(np.array(data), no_dims=argp.no_dims, perplexity=argp.perplexity, theta=argp.theta, 
+                          randseed=argp.randseed, verbose=argp.verbose, initial_dims=argp.initial_dims):
+        argp.output.write("\t".join(map(str, result)) + "\n")
+
+    return 0
 
 if __name__ == '__main__':
     from sys import argv
