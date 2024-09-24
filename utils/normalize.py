3840ec58beb3581456a921a5399b865f56d5e964
Doug Goodwin <dgoodwin@gmail.com>
2024-09-24 10:38:42 -0700

    Python3

--- utils/normalize.py
+++ utils/normalize.py
@@ -1,4 +1,11 @@
+# def normalize(X):
+# 	X -= X.min(axis=0)
+# 	X /= X.max(axis=0)
+# 	return X
+# 
+import numpy as np
+
 def normalize(X):
-    X -= X.min(axis=0)
-    X /= X.max(axis=0)
+    X = X - np.min(X, axis=0)
+    X = X / np.ptp(X, axis=0)
     return X
