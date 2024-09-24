ce433bb9e59abe28ddae72a891f0b975648dce4f
Doug Goodwin <dgoodwin@gmail.com>
2024-09-24 10:39:03 -0700

    Python3

--- utils/show_array.py
+++ utils/show_array.py
@@ -1,16 +1,24 @@
-from cStringIO import StringIO
+from io import BytesIO
 import numpy as np
-import PIL.Image
+from PIL import Image
 import IPython.display
+import imageio
 import shutil
 
 def show_array(a, fmt='png', filename=None):
     a = np.uint8(np.clip(a, 0, 255))
-    image_data = StringIO()
-    PIL.Image.fromarray(a).save(image_data, fmt)
+    print(f"a has type {type(a)}")
+    
+    image_data = BytesIO()
+    print(f"write image 'output.png'")
+    imageio.imwrite("output.png", a, format="PNG")
+    
+    image = Image.fromarray(a)
+    image.save(image_data, fmt)
+    
     if filename is None:
         IPython.display.display(IPython.display.Image(data=image_data.getvalue()))
     else:
-        with open(filename, 'w') as f:
+        with open(filename, 'wb') as f:
             image_data.seek(0)
             shutil.copyfileobj(image_data, f)
